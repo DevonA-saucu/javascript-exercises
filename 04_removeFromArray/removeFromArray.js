d1d9bb7492d4807259e83bb6bfe50d13be639d29
Devon <60799686+DevonA-saucu@users.noreply.github.com>
2024-07-12 21:51:18 -0500

    Make removeFromArray function work correctly

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removal) {
+    let newArray = array;
+
+    for (let j = 0; j < removal.length; j++) {
+
+        for (let i = 0; i < newArray.length; i++) {
+            if (array[i] === removal[j]) {
+                newArray.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray;   
