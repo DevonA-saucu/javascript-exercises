be804318c8ee588e650b58ac2b42eaf28af9fbd4
Devon <60799686+DevonA-saucu@users.noreply.github.com>
2024-07-12 22:05:15 -0500

    Make sumAll function work correctly

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+    if (intOne < 0 || intTwo < 0 || typeof(intOne) !== "number" || typeof(intTwo) !== "number") {
+        return "ERROR";
+    }
 
+    let sum = 0;
+
+    if (intOne > intTwo) {
+        for (let i = intOne; i >= intTwo; i--) {
+            sum += i;
+        }
+    }
+
+    for (let i = intOne; i <= intTwo; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
