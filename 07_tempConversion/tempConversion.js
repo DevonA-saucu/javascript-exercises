405dc8fddc6931df26d90974b38048ffabf3c8e7
Devon <60799686+DevonA-saucu@users.noreply.github.com>
2024-07-12 22:53:31 -0500

    Make tempConversion and leapYears functions work properly

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+const FIVE_OVER_NINE = 5 / 9;
+const NINE_OVER_FIVE = 9 / 5;
+
+const convertToCelsius = function(temp) {
+  if (temp === 32) {
+    return 0;
+  }
+  let fahrenheit = temp - 32;
+  fahrenheit *= FIVE_OVER_NINE;
+  fahrenheit = Math.round(fahrenheit * 10) / 10
+  return fahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  if (temp === 0) {
+    return 32;
+  }
+  let celsius = temp * NINE_OVER_FIVE;
+  celsius += 32;
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
 // Do not edit below this line
