8c83a29feb818e19f3f3cf5d40f450f5b76b8384
Devon <60799686+DevonA-saucu@users.noreply.github.com>
2024-07-15 16:19:08 -0500

    Make all calculator functions work properly

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+  return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  let arrSum = 0;
+  if (numArray.length === 0) {
+    return arrSum;
+  }
+
+	arrSum = numArray.reduce((total, currentNum) => {
+    return total + currentNum;
+  });
+
+  return arrSum;
 };
 
-const multiply = function() {
+const multiply = function(numArray) {
+  let arrMul = numArray.reduce((total, currentNum) => {
+    return total * currentNum;
+  });
 
+  return arrMul;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factorialSum = 1
+
+	if (number === 0) {
+    return factorialSum;
+  }
+
+  while (number > 0) {
+    factorialSum *= number;
+    number--;
+  }
+  
+  return factorialSum;
 };
 
 // Do not edit below this line
