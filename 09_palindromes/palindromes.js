a9991a936dacd354e481340cbc065c770054ca4b
Devon <60799686+DevonA-saucu@users.noreply.github.com>
2024-07-15 17:29:30 -0500

    Make palindromes and fibonacci functions work

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const punctuationArr = [".", ",", "!", "?"];
 
+const palindromes = function (word) {
+    modified = replacePunctuation(word);
+    reversed = modified.split("").reverse().join("");
+
+    console.log(modified);
+
+    return modified.toLowerCase() === reversed.toLowerCase()
 };
+
+function replacePunctuation(string) {
+    for (punctuation of punctuationArr) {
+        string = string.replaceAll(punctuation, " ");
+    }
+    
+    let newString = string.split(" ").join("");
+
+    return newString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = +index;
 
-};
+    if (index === 0 ) {
+        return 0;
+    }
+
+    if (index < 0) {
+        return "OOPS";
+    }
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= index; i++) {
+        let currentIndex = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = currentIndex;
+    }
+
+    return firstPrev;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
